29be79878ee43ce276e784855a73d2ffa3034d72
Timothy Wood <tim@imaginesimplicity.com>
2014-11-14 07:27:22 -0500

    adding product descriptions

--- template/dashboard.php
+++ template/dashboard.php
@@ -1,13 +1,13 @@
 <h2>Getting Started <a href="#" id="legull_ajax_generate_docs" class="add-new-h2">Generate Documents</a></h2>
-<!-- <div id="poststuff">
+<div id="poststuff">
 	<div class="postbox-container">
 		<div class="postbox addon">
 			<h3 class="hndle">
-				<span>Title</span>
+				<span>Welcome to Legull</span>
 			</h3>
 			<div class="inside">
-				<h4>Description:</h4>
+				<p>Answering the questions on each of these topic tabs will generate your site’s Terms of Service and Privacy Policy for you. Additional terms to address things like E-commerce terms, User Generated Content and DMCA terms, and other aspects of your site are available as add-ons at <a href="http://www.legull.com" target="blank">legull.com</a>.</p>
 			</div>
 		</div>
 	</div>
-</div> -->
+</div>
